f1c4b4cd357aeb3471f656a1d2616c1e150534dc
markogarza <markogarza@gmail.com>
2023-05-23 12:03:59 -0400

    Files done

--- index.js
+++ index.js
@@ -19,19 +19,73 @@ const writeFilePro = (file, data) => {
   });
 };
 
-readFilePro(`${__dirname}/dog.txt`)
-  .then((data) => {
+const getDogPic = async () => {
+  try {
+    const data = await readFilePro(`${__dirname}/dog.txt`);
     console.log(`Breed: ${data}`);
-    return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
-  })
-  .then((res) => {
-    console.log(res.body);
-    return writeFilePro('dog-img.txt', res.body.message);
-  })
-  .then(() => console.log('Success'))
-  .catch((err) => {
+
+    const res1pro = superagent.get(
+      `https://dog.ceo/api/breed/${data}/images/random`
+    );
+    const res2pro = superagent.get(
+      `https://dog.ceo/api/breed/${data}/images/random`
+    );
+    const res3pro = superagent.get(
+      `https://dog.ceo/api/breed/${data}/images/random`
+    );
+
+    const all = await Promise.all([res1pro, res2pro, res3pro]);
+    const imgs = all.map((el) => el.body.message);
+    console.log(imgs);
+
+    await writeFilePro('dog-img.txt', imgs.join('\n'));
+    console.log('File written');
+  } catch (err) {
     console.log(err.message);
-  });
+    throw err;
+  }
+
+  return '2: Ready doggy';
+};
+
+(async () => {
+  try {
+    console.log('1: Getting doggy');
+    const x = await getDogPic();
+    console.log(x);
+    console.log('3: Ready number 3');
+  } catch (err) {
+    console.log('New Error!');
+  }
+})();
+
+// console.log('1: Getting doggy');
+// getDogPic()
+//   .then((x) => {
+//     console.log(x);
+//     console.log('3: Ready number 3');
+//   })
+//   .catch((err) => {
+//     console.log('New Error!');
+//   });
+
+// Second solution
+
+// readFilePro(`${__dirname}/dog.txt`)
+//   .then((data) => {
+//     console.log(`Breed: ${data}`);
+//     return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
+//   })
+//   .then((res) => {
+//     console.log(res.body);
+//     return writeFilePro('dog-img.txt', res.body.message);
+//   })
+//   .then(() => console.log('Success'))
+//   .catch((err) => {
+//     console.log(err.message);
+//   });
+
+//   First solution
 
 // fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
 //   console.log(`Breed: ${data}`);
